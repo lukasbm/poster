106b3a95a628db2bd70869897f9a5932e3482cfb
Lukas Boehm <lukas.l.boehm@fau.de>
2024-09-17 19:54:14 +0200

    fix :bug:

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Poster
 
+## Features
+
+- run, rerun and continue experiments
+
+## CLI
+
+TODO
+
+## Callbacks
+
+Callbacks can server all sorts of purposes. They can be used to save the model, log metrics, etc.
+
+- [X] Hydra Zen (argument parsing)
+- [ ] Hydra (argument parsing)
+- [ ] Aim (Experiment Tracker)
+- [ ] Python Fire (argument parsing)
+- [ ] Local Directory (data storage)
+- [ ] S3 (data storage)
+- [ ] Weights & Biases (Experiment Tracker)
+- [ ] SCP (data storage)
+- [ ] Submitit (Job submission)
+- [ ] Logging (logging)
--- examples/simple.py
+++ examples/simple.py
@@ -1,4 +1,5 @@
 from typing import List
+
 from hydra_zen import store, just
 
 from poster import Launcher, HydraZenCallback
@@ -16,10 +17,12 @@ def main_fn(a: int, names: List[str]) -> int:
 
 
 if __name__ == "__main__":
-    print("simple")
+    print("simple example!")
 
+    # build hydra stores
     name_store = store(group="names")
     name_store(just(["Alice", "Bob", "Charlie"]), name="basic")
+    name_store(just(["Dave", "Eve", "Frank"]), name="advanced")
 
     launcher = Launcher(callbacks=[
         HydraZenCallback(task_fn=main_fn, store=store, job_type="simple"),
